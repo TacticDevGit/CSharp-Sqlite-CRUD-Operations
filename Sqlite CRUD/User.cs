46792233de543b9c24879fcac1f6c1c61630f5fb
Tactic Dev <38791845+TacticDevGit@users.noreply.github.com>
2021-08-16 19:30:12 +0200

    Add files via upload

---
+++ Sqlite CRUD/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sqlite_CRUD
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
